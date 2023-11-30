14b776a1b68feba3be0e820a8581cabc7568815b
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-01 00:09:09 +0700

    Add route to book ticket and save data to database

--- src/routes/bookTicketRoute.js
+++ src/routes/bookTicketRoute.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const bookings = require("../../js/bookings");
+const bookTicket = require("../../model/book_ticket");
 
-router.post("/", (req, res) => {
-  let ticketNumber = Math.floor(Math.random() * 1000000000);
-  bookings[ticketNumber] = req.body;
-  res.json({ ticketNumber: ticketNumber });
+router.post("/", async (req, res) => {
+  try {
+    await bookTicket.create({
+      id: 123456,
+      name: req.body.name,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      message: req.body.message,
+      destination: req.body.destination,
+      quantity: req.body.quantity,
+      arrivalDate: req.body.arrivalDate,
+      leavingDate: req.body.leavingDate,
+    });
+    res.send({ message: "Data successfully added" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Failed to add data" });
+  }
 });
 
 module.exports = router;
