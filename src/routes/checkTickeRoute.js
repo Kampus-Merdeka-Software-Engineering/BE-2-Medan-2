d52e7ea334a1e402429f9b3333d9a67be6aa5846
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-01 00:09:22 +0700

    Add console.log to check newTicket object

--- src/routes/checkTickeRoute.js
+++ src/routes/checkTickeRoute.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const bookings = require("../../js/bookings");
 
 router.get("/:ticketNumber", (req, res) => {
+  let newTicket = { nama: "John" };
+  // console.log(newTicket.nama);
+  console.log(newTicket["nama"]);
   let ticketNumber = req.params.ticketNumber;
   if (bookings[ticketNumber]) {
     res.json(bookings[ticketNumber]);
