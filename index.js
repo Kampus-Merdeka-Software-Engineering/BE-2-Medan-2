f4ef7e239591d208985ff0cf8ac06874e1bad4d8
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-02 14:30:43 +0700

    Add cors and sequelize sync

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT;
 
 const bookTicketRoute = require("./src/routes/bookTicketRoute");
 const checkTicketRoute = require("./src/routes/checkTickeRoute");
 const database = require("./database");
+const sequelize = require("./database");
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/book-ticket", bookTicketRoute);
 app.use("/check-ticket", checkTicketRoute);
+
+sequelize.sync({ alter: true });
 
 app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
