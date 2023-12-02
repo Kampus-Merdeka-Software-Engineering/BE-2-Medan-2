07d73710f6b282b7a22c4364ebfa3bb80a903d20
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-02 14:40:23 +0700

    Update sequelize import in book_ticket.js

--- model/book_ticket.js
+++ model/book_ticket.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = require("../database");
+const sequelize = require("../src/config/dbConfig");
 
 const bookTicket = sequelize.define(
   "bookTicket",
