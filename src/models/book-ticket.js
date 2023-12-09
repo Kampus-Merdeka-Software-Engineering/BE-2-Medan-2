09c49a521cb4a67bbcfb84962911a13b02d98624
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-09 18:15:01 +0700

    docs(model): add comments for clarity

--- src/models/book-ticket.js
+++ src/models/book-ticket.js
@@ -1,9 +1,13 @@
+// Importing required modules from Sequelize
 const { Sequelize, DataTypes } = require("sequelize");
+// Importing the configured Sequelize instance
 const sequelize = require("../config/dbConfig");
 
+// Defining the 'bookTicket' model using the 'define' method of the Sequelize instance
 const bookTicket = sequelize.define(
   "bookTicket",
   {
+    // Defining the fields of the 'bookTicket' model
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -44,9 +48,12 @@ const bookTicket = sequelize.define(
     },
   },
   {
+    // Disabling timestamps (createdAt and updatedAt fields)
     timestamps: false,
+    // Specifying the table name in the database
     tableName: "book_ticket",
   }
 );
 
+// Exporting the 'bookTicket' model to be used in index.js
 module.exports = bookTicket;
